96cc34ba76187e4bf0fb1f0c5a7898a2bf8973a7
= <=>
2022-01-07 17:57:02 -0300

    tentativa api

--- src/App.js
+++ src/App.js
@@ -1,10 +1,39 @@
 
-import React from 'react';
-import Test from './components/Teste';
+import React, { useState, useEffect } from 'react';
+import { View, Text } from 'react-native';
+
+const axios = require('axios').default;
+const api = axios.create({
+    baseUrl: 'http://localhost:3000',
+});
 
 const App = () => {
+    const [events, setEvents] = useState([{title: 'null'}]);
+
+    useEffect(() => {
+        try {
+            api.get('api/events')
+                .then(({data}) => {
+                    setEvents(data);
+                    // console.warn(data);
+            })
+                .catch(function (error) {
+                    console.warn(error);
+            });
+        } catch (error) {
+            console.warn(error);
+
+        }
+    }, []);
+
     return (
-        <Test text="Emerson 31"/>
+        <View>
+            {
+                events?.map((item, index) => (
+                    (<Text key={index}>{item.title}</Text>)
+                ))
+            }
+        </View>
     );
 };
 
